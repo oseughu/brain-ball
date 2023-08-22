bfeb595366d9278d1ac4e2c1c4fc887219e8595e
Ose Ughu <oseughu@gmail.com>
2023-08-22 20:51:35 +0100

    set tokens to id, not uuid

--- database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('failed_jobs', function (Blueprint $table) {
-            $table->uuid('id')->primary();
+            $table->id();
             $table->text('connection');
             $table->text('queue');
             $table->longText('payload');
