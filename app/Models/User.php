fe85bf23e11823a409210d933cd1b46ad949e4d2
Ose Ughu <oseughu@gmail.com>
2023-08-31 14:41:59 +0100

    switch to prefixed ulids

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,8 +3,6 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-
-use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -12,7 +10,16 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, HasUuids;
+    use HasApiTokens, HasFactory, Notifiable;
+
+    public static function boot()
+    {
+        parent::boot();
+
+        self::creating(function ($model) {
+            $model->id = $model->role . '_' . (\Str::ulid());
+        });
+    }
 
     /**
      * The attributes that are mass assignable.
